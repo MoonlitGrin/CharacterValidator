6b1076ace52f1448124ff4ab62ba9c4eac0a4521
Moon <MoonlitSmirk@gmail.com>
2025-11-11 10:58:47 +0000

    Calculations & While Statements.

---
+++ Character Validator/Program.cs
@@ -0,0 +1,31 @@
+// Variables.
+int strength = 0;
+int intelligence = 0;
+
+int bonusStrength = 0;
+int bonusIntelligence = 0;
+// Introduction & Instructions.
+Console.WriteLine("Hello, please submit a number between 1-20 for your character's strength.");
+strength = Convert.ToInt32(Console.ReadLine());
+
+while (strength < 1 || strength > 20) 
+{
+    Console.WriteLine("An error has occured, please re-sumbit your strength.");
+    strength = Convert.ToInt32(Console.ReadLine());
+};
+bonusStrength = (strength - 10) / 2;
+Console.WriteLine("Strength Bonus:");
+Console.WriteLine(bonusStrength);
+
+Console.WriteLine("Please submit a number between 1-20 for your character's intelligence.");
+intelligence = Convert.ToInt32(Console.ReadLine());
+
+while (intelligence < 1 || intelligence > 20)
+{
+    Console.WriteLine("An error has occured, please re-submit your intelligence.");
+    intelligence = Convert.ToInt32(Console.ReadLine());
+};
+bonusIntelligence = (intelligence - 10) / 2;
+Console.WriteLine("Intelligence Bonus:");
+Console.WriteLine(bonusIntelligence);
+// Calculations.
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# CharacterValidator
-College HNC - Programming Project.
+Hai gang!
+...I dont know what to put here
+I just wanted to yap
+
+Shout out to my computer science teacher for teaching me all of this
+He is super silly
+
+
+Okay bye gang.
