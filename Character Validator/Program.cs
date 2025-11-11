a70a730ca2a039d6ec832b0c29afbc7d1f58bd48
Moon <MoonlitSmirk@gmail.com>
2025-11-11 11:25:30 +0000

    Working code.
    Needs to follow psuedocode but the general code works.

--- Character Validator/Program.cs
+++ Character Validator/Program.cs
@@ -8,11 +8,12 @@ int bonusIntelligence = 0;
 Console.WriteLine("Hello, please submit a number between 1-20 for your character's strength.");
 strength = Convert.ToInt32(Console.ReadLine());
 
+// Stats (while statements).
 while (strength < 1 || strength > 20) 
 {
-    Console.WriteLine("An error has occured, please re-sumbit your strength.");
+    Console.WriteLine("An error has occured, please re-sumbit your strength and ensure it is between 1-20.");
     strength = Convert.ToInt32(Console.ReadLine());
-};
+}
 bonusStrength = (strength - 10) / 2;
 Console.WriteLine("Strength Bonus:");
 Console.WriteLine(bonusStrength);
@@ -22,10 +23,15 @@ intelligence = Convert.ToInt32(Console.ReadLine());
 
 while (intelligence < 1 || intelligence > 20)
 {
-    Console.WriteLine("An error has occured, please re-submit your intelligence.");
+    Console.WriteLine("An error has occured, please re-submit your intelligence and ensure it is between 1-20.");
     intelligence = Convert.ToInt32(Console.ReadLine());
-};
+}
 bonusIntelligence = (intelligence - 10) / 2;
 Console.WriteLine("Intelligence Bonus:");
 Console.WriteLine(bonusIntelligence);
 // Calculations.
+if (bonusStrength > 1)
+    Console.WriteLine("Your character is qualified to be a Warrior!");
+
+if (bonusIntelligence > 1)
+    Console.WriteLine("Your character is qualified to be a Mage!");
