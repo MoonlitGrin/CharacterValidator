3fa208d5325a1c8c24b6c3b6ffa59470867d8f5c
Moon <MoonlitSmirk@gmail.com>
2025-11-11 12:38:34 +0000

    Pseudocode Variables
    Fixing Variable names.

--- Character Validator/Program.cs
+++ Character Validator/Program.cs
@@ -2,8 +2,8 @@
 int strength = 0;
 int intelligence = 0;
 
-int bonusStrength = 0;
-int bonusIntelligence = 0;
+int strengthBonus = 0;
+int intelligenceBonus = 0;
 // Introduction & Instructions.
 Console.WriteLine("Hello, please submit a number between 1-20 for your character's strength.");
 strength = Convert.ToInt32(Console.ReadLine());
@@ -14,8 +14,8 @@ while (strength < 1 || strength > 20)
     Console.WriteLine("An error has occured, please re-sumbit your strength and ensure it is between 1-20.");
     strength = Convert.ToInt32(Console.ReadLine());
 }
-bonusStrength = (strength - 10) / 2;
-Console.WriteLine("Strength Bonus: " + bonusStrength);
+strengthBonus = (strength - 10) / 2;
+Console.WriteLine("Strength Bonus: " + strengthBonus);
 
 
 Console.WriteLine("Please submit a number between 1-20 for your character's intelligence.");
@@ -26,11 +26,12 @@ while (intelligence < 1 || intelligence > 20)
     Console.WriteLine("An error has occured, please re-submit your intelligence and ensure it is between 1-20.");
     intelligence = Convert.ToInt32(Console.ReadLine());
 }
-bonusIntelligence = (intelligence - 10) / 2;
-Console.WriteLine("Intelligence Bonus: " + bonusIntelligence);
+intelligenceBonus = (intelligence - 10) / 2;
+Console.WriteLine("Intelligence Bonus: " + intelligenceBonus);
+
 // Calculations.
-if (bonusStrength > 1)
+if (strengthBonus > 1)
     Console.WriteLine("Your character is qualified to be a Warrior!");
 
-if (bonusIntelligence > 1)
+if (intelligenceBonus > 1)
     Console.WriteLine("Your character is qualified to be a Mage!");
