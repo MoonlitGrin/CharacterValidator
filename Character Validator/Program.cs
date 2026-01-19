f55792423b25d805bd8360ff849c3c2273f53a66
Moon <MoonlitSmirk@gmail.com>
2026-01-19 16:32:18 +0000

    Final with Functions

--- Character Validator/Program.cs
+++ Character Validator/Program.cs
@@ -1,4 +1,4 @@
-// Variables.
+// Global Variables.
 using System.Net.Security;
 
 int strength = 0;
@@ -7,56 +7,47 @@ int intelligence = 0;
 int strengthBonus = 0;
 int intelligenceBonus = 0;
 
-// Introduction & Instructions.
-Console.WriteLine("Hello, please submit a number between 1-20 for your character's strength.");
-strength = Convert.ToInt32(Console.ReadLine());
-
-bool strengthValid = strength >= 1 && strength <= 20;
-
-while(strengthValid == false) 
+// Functions.
+int GetStatValue(string statName)
 {
-    Console.WriteLine("An error has occured, please re-sumbit your strength and ensure it is between 1-20.");
-    strength = Convert.ToInt32(Console.ReadLine());
-
-    strengthValid = (strength >= 1 && strength <= 20);
+    int statValue = 0;
+    Console.WriteLine("Please enter the stats for " + statName);
+    statValue = Convert.ToInt32(Console.ReadLine());
+    while (IsStatValid(statValue) == false)
+    {
+        Console.WriteLine("The following is invalid, please re-enter.");
+        statValue = Convert.ToInt32(Console.ReadLine());
+    }
+    return statValue;
+}
+bool IsStatValid(int statValue)
+{
+    bool valid = true;
+    if (statValue < 1 || statValue > 20)
+    {
+        valid = false;
+    }
+    return valid;
+}
+int CalculateStatBonus(int statValue)
+{
+    int statBonus = 0;
+    statBonus = (statValue - 10) / 2;
+    return statBonus;
 }
 
+// Global Code.
+strength = GetStatValue("strength");
+intelligence = GetStatValue("intelligence");
 
-Console.WriteLine("Please submit a number between 1-20 for your character's intelligence.");
-intelligence = Convert.ToInt32(Console.ReadLine());
+// Calculations.
+strengthBonus = CalculateStatBonus(strength);
+Console.WriteLine("The stat bonus for strength is " + strengthBonus);
+intelligenceBonus = CalculateStatBonus(intelligence);
+Console.WriteLine("The stat bonus for intelligence is " + intelligenceBonus);
 
-bool intelligenceValid = intelligence >= 1 && intelligence <= 20;
-
-while(intelligenceValid == false)
-{
-    Console.WriteLine("An error has occured, please re-submit your intelligence and ensure it is between 1-20.");
-    intelligence = Convert.ToInt32(Console.ReadLine());
-
-    intelligenceValid = (intelligence >= 1 && intelligence <= 20);
-}
-
-
-// Stats (while statements).
-strengthBonus = (strength - 10) / 2;
-Console.WriteLine("Strength Bonus: " + strengthBonus);
-
-intelligenceBonus = (intelligence - 10) / 2;
-Console.WriteLine("Intelligence Bonus: " + intelligenceBonus);
-
-
-// Selection
-
+// Qualification.
 if (strengthBonus >= 1)
-{
-    Console.WriteLine("Character qualifies to be a Warrior.");
-}
-
+    Console.WriteLine("You qualify to be a warrior.");
 if (intelligenceBonus >= 1)
-{
-    Console.WriteLine("Character qualifies to be a Mage.");
-}
-
-if (strengthBonus < 1 && intelligenceBonus < 1)
-{
-    Console.WriteLine("Character does not qualify for any class.");
-}
+    Console.WriteLine("You qualify to be a mage.");
